aaf772569abe9aa7b2ee610db995a60d79857f9e
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-04 00:47:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7959445535:AAH6cQtHpNBDfx8b8MGkdrxeM3imLZ1LFHg")
     API_ID = int(environ.get("API_ID", "29259501"))
     API_HASH = environ.get("API_HASH", "290ab5e9ce678bbf1151b4d5072d0db2")
     AUTH_USERS = 6290395209"""
