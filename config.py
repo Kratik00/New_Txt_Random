b858630e24f4421356e512253065954df8a2383a
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-04 01:36:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 # (c) ACE 
 
-from os import environ
+import os
 
-API_ID = int(environ.get("API_ID", "2925950"))
-API_HASH = environ.get("API_HASH", "290ab5e9ce678bbf1151b4d5072d0db2")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-AUTH_USERS = 6290395209
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7959445535:AAH6cQtHpNBDfx8b8MGkdrxeM3imLZ1LFHg")
+    API_ID = int(os.environ["API_ID", 29259501]
+    API_HASH = os.environ["API_HASH", "290ab5e9ce678bbf1151b4d5072d0db2"]
+    AUTH_USERS = "6290395209"""
